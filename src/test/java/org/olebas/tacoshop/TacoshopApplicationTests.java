4a81420fb96aaf8757baa28a794944af918d450c
onevoit <onevoit@itsmartflex.com>
2022-06-03 14:05:13 +0300

    init

---
+++ src/test/java/org/olebas/tacoshop/TacoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.olebas.tacoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
